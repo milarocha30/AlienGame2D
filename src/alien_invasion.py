061b1ebaf9cf88286b6ab77f6f98385e6bf81ddb
Camila <milaurocha@gmail.com>
2026-08-25 10:31:44 -0300

    refatoração do código: aplicando SRP

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -168,6 +168,106 @@ class AlienInvasion:
                 )  # Imprime uma mensagem no console indicando que a nave foi atingida
                 sys.exit()  # Encerra o jogo
 
+    def _check_events(self):
+        """Responde a eventos de pressionamento de teclas e mouse (fechamento da janela)"""
+        for event in pygame.event.get():
+            if  event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._handle_keydown(event)
+            elif event.type == pygame.KEYUP:
+                self._handle_keyup(event)
+    def _handle_keydown(self, event: pygame.event.Event) -> None:
+        """Responde a eventos de pressionamento de teclas."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+
+    def _handle_keyup(self, event: pygame.event.Event) -> None:
+        """Responde a eventos de soltura de teclas."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _fire_bullet(self) -> None:
+        """Dispara um projétil se o limite de projéteis ainda não tiver sido alcançado."""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self.screen, self.settings, self.ship)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self) -> None:
+        """Atualiza a posição dos projéteis e se livra dos projéteis antigos."""
+        self.bullets.update()
+        self._remove_offscreen_bullets()
+        self._check_bullet_alien_collisions()
+
+
+    def _remove_offscreen_bullets(self) -> None:
+        """Remove os projéteis que desapareceram da tela."""
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+
+    def _check_bullet_alien_collisions(self) -> None:
+            """Verifica colisões entre projéteis e alienígenas."""
+            pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+
+    def _update_aliens(self) -> None:
+        """Verifica se a frota de alienígenas está em uma borda, então atualiza as posições de todos os alienígenas na frota."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+
+    def _check_fleet_edges(self) -> None:
+        """Responde apropriadamente se algum alienígena tiver alcançado uma borda."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self) -> None:
+        """Desce a frota e muda sua direção."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+    def _check_ship_collision(self) -> None:
+        """Verifica se a nave colidiu com algum alienígena."""
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("A nave foi atingida.")
+            sys.exit()
+
+
+    def _render_screen(self) -> None:
+        """Redesenha a tela a cada passagem pelo laço."""
+        self.screen.fill(self._bg_color)
+        self.ship.blitme()
+        self.aliens.draw(self.screen)
+        self._draw_bullets()
+        pygame.display.flip()
+
+
+    def _draw_bullets(self) -> None:
+        """Desenha os projéteis na tela."""
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+
+    def _update_game_state(self) -> None:
+        """Atualiza a posição da nave, dos projéteis e dos alienígenas."""
+        self.ship.update()
+        self._update_bullets()
+        self._update_aliens()
+        self._check_ship_collision()
+
 
 if __name__ == "__main__":
     alien_invasion = AlienInvasion()
