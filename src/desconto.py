337648d48ee739b50587778f6deb2d69f3f1b0a5
Camila <milaurocha@gmail.com>
2026-09-03 10:35:42 -0300

    Refatoração: aplicando princípio SOLID

--- src/desconto.py
+++ src/desconto.py
@@ -1,29 +1,52 @@
 from abc import ABC, abstractmethod
 
-class Desconto(ABC):
+class IDesconto(ABC):
     @abstractmethod
     def calcular(self, valor):
         pass
 
-class Normal(Desconto):
+class ICupom:
+    def aplicar_cupom(self, codigo):
+        raise NotImplementedError
+    
+class IVIP:
+    def validar_usuario_vip(self, usuario):
+        raise NotImplementedError
+
+
+class Normal(IDesconto):
     def calcular(self, valor):
         return valor * 0.1
 
-class Vip(Desconto):
+class Vip(IDesconto, ICupom, IVIP):
     def calcular(self, valor):
         return valor * 0.2
+    
+    def aplicar_cupom(self, valor):
+        return True
+    
+    def validar_usuario_vip(self, usuario):
+        return usuario == "vip"
 
-class Premium(Desconto):
+class Premium(IDesconto):
     def calcular(self, valor):
         return valor * 0.3 
+
+def aplicar_desconto(desconto: IDesconto, valor : float) -> float:  
+    return desconto.calcular(valor)
+
+def aplicar_cupom(cupom: ICupom, codigo : str) -> bool:
+    return cupom.aplicar_cupom(codigo)
 
 def main():
     valor = 100
 
     normal = Normal()
     vip = Vip()
-    premium = Premium()
 
-    print(f"Desconto normal: R$ {normal.calcular(valor):.2f}")
-    print(f"Desconto vip: R$ {vip.calcular(valor):.2f}")
-    print(f"Desconto premium: R$ {premium.calcular(valor):.2f}")
+    print(f"Desconto normal: R$ {aplicar_desconto(normal, valor):.2f}")
+    print(f"Desconto vip: R$ {aplicar_desconto(vip, valor):.2f}")
+    print("Cupom VIP:", aplicar_cupom(vip, "DESC10"))    # porque recebe vip nessa função??
+
+if __name__ == "__main__":
+    main()
